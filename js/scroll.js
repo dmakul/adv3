0c6a0226671582bccef02c8705285d7979331a80
dmakul <makuldauren@gmail.com>
2015-08-17 22:47:43 +0600

    version 1

---
+++ js/scroll.js
@@ -0,0 +1,10 @@
+$(document).ready(function() {
+    $("a.topLink").click(function() {
+        $("html, body").animate({
+            scrollTop: $($(this).attr("href")).offset().top + "px"
+        }, {
+            duration: 500
+        });
+        return false;
+    });
+});
